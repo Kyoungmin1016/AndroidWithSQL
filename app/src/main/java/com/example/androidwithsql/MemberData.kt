bc8587685ebe8c0ec9e31cd3af4e883f03e4c5e3
lvlz-chal <rlacofl1000@gmail.com>
2021-11-29 14:46:37 +0900

    좌석표

--- app/src/main/java/com/example/androidwithsql/MemberData.kt
+++ app/src/main/java/com/example/androidwithsql/MemberData.kt
@@ -1,3 +1,5 @@
 package com.example.androidwithsql
 
 data class MemberData(var M_id : String, var M_password: String, var name: String, var phoneNo: String)
+
+data class Seat( val imageResource: Int, val text1: String, var text2: String)
